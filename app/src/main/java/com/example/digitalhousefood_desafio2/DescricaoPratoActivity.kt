aabb5cc6a5693ef8fd0e51339159de41bcf730ee
Mariana Marcelli <marimmarcelli@gmail.com>
2020-11-13 12:24:34 -0300

    Adicionando botão voltar

--- app/src/main/java/com/example/digitalhousefood_desafio2/DescricaoPratoActivity.kt
+++ app/src/main/java/com/example/digitalhousefood_desafio2/DescricaoPratoActivity.kt
@@ -7,5 +7,6 @@ class DescricaoPratoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_descricao_prato)
+        this.title = ""
     }
 }
